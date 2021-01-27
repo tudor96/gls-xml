4e52c7f7655af319d476cd6e9c9439dd76f4047e
Valentin Gorcea <valentingorcea@gmail.com>
2021-01-27 22:11:59 +0200

    add fetch with gls data

--- app.js
+++ app.js
@@ -1,32 +1,53 @@
-var fs = require("fs"),
-  xml2js = require("xml2js");
+var xml2js = require("xml2js");
+var axios = require("axios");
 
-
-  function processWrongTags(name){
-    return name.replace("soap:",'').replace("$",'item');
+function processWrongTags(name) {
+  return name.replace("soap:", '').replace("$", 'item');
 }
 
-  function processWrongAttrs(name){
-    return name.replace("$",'item');
+function processWrongAttrs(name) {
+  return name.replace("$", 'item');
 }
 
-var parser = new xml2js.Parser({trim: true, normalizeTags: true, tagNameProcessors: [processWrongTags], attrValueProcessors: [processWrongAttrs]});
+const xmlBodyRequest = `<soap:Envelope
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+<soap:Body>
+      <execRIA
+             xmlns="http://asw.ro/">
+             <sesiune>site</sesiune>
+           <proceduraSQL>SNCwExportArticoleMagazinOnline</proceduraSQL>
+             <strParXML></strParXML>
+             <reqTimeOut>999</reqTimeOut>
+      </execRIA>
+</soap:Body>    
+</soap:Envelope>
+`;
 
-fs.readFile(__dirname + "/endpoint.xml", function (err, data) {
-  parser.parseString(data, function (err, result) {
-    const finalObj = result.envelope.body[0].execriaresponse[0].execriaresult[0].sqlxml[0];
-    const produse = finalObj.date[0].articole[0].articol;
-    const produseProcessed = produse.map(produs => {
-        const array =  Object.entries(produs).map(([name, value]) => value)
-        return array[0];
-    });
-    console.log("Date",finalObj.date);
-    console.log("Mesaje",finalObj.mesaje);
-    console.log("Errori",finalObj.date);
-    console.log(produseProcessed)
-
-
-
-    console.log("Done");
+let getProducts = async () => {
+  let response = await axios.post('http://asis.glissando.ro:8081/asis/mobria/dateria.asmx', xmlBodyRequest, {
+    headers: {
+      'Content-Type': 'text/xml; charset=utf-8',
+      'Host': 'asis.glissando.ro',
+      'SOAPAction': 'http://asw.ro/execRIA',
+      'Connection': 'keep-alive',
+      'Accept-Encoding': 'gzip, deflate, br'
+    }
   });
-});
+  var parser = new xml2js.Parser({ trim: true, normalizeTags: true, tagNameProcessors: [processWrongTags], attrValueProcessors: [processWrongAttrs] });
+      parser.parseString(response.data, function (err, result) {
+        const finalObj = result.envelope.body[0].execriaresponse[0].execriaresult[0].sqlxml[0];
+        const produse = finalObj.date[0].articole[0].articol;
+        const produseProcessed = produse.map(produs => {
+          const array = Object.entries(produs).map(([name, value]) => value)
+          return array[0];
+        });
+        console.log("Date", finalObj.date);
+        console.log("Mesaje", JSON.stringify(finalObj.mesaje));
+        console.log("Errori", finalObj.erori);
+        console.log(produseProcessed)
+      });
+}
+
+getProducts();
