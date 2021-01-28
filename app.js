21b20957de2ede88657e744688836e0f45ae4adf
Valentin Gorcea <valentingorcea@gmail.com>
2021-01-28 18:02:19 +0200

    insert product in site

--- app.js
+++ app.js
@@ -57,116 +57,115 @@ let getProducts = async () => {
 
   //console.log("categories: ", categories.data[0])
   var parser = new xml2js.Parser({ trim: true, normalizeTags: true, tagNameProcessors: [processWrongTags], attrValueProcessors: [processWrongAttrs] });
-      parser.parseString(response.data, async function (err, result) {
-        const finalObj = result.envelope.body[0].execriaresponse[0].execriaresult[0].sqlxml[0];
-        const produse = finalObj.date[0].articole[0].articol;
-        const produseProcessed = produse.map(produs => {
-          const array = Object.entries(produs).map(([name, value]) => value)
-          return array[0];
-        });
-       /*  console.log("Date", finalObj.date);
-        console.log("Mesaje", JSON.stringify(finalObj.mesaje));
-        console.log("Errori", finalObj.erori);
-        console.log(produseProcessed)  */
-        produseProcessed[0].sku = 'FER-002'; 
+  parser.parseString(response.data, async function (err, result) {
+    const finalObj = result.envelope.body[0].execriaresponse[0].execriaresult[0].sqlxml[0];
+    const produse = finalObj.date[0].articole[0].articol;
+    const produseProcessed = produse.map(produs => {
+      const array = Object.entries(produs).map(([name, value]) => value)
+      return array[0];
+    });
+    /*  console.log("Date", finalObj.date);
+     console.log("Mesaje", JSON.stringify(finalObj.mesaje));
+     console.log("Errori", finalObj.erori);
+     console.log(produseProcessed)  */
+    produseProcessed[0].sku = 'FER-002';
 
-        produseProcessed.forEach(element => {
-          if(categories.data.find(x => x.name.toUpperCase() === element.denumire_grupa.toUpperCase())) {
-          }
-        });
+    produseProcessed.forEach(element => {
+      if (categories.data.find(x => x.name.toUpperCase() === element.denumire_grupa.toUpperCase())) {
+      }
+    });
 
-        /* let promises = produseProcessed.map(element => {
-          return axios.get(`https://www.casaplant.ro/wp-json/wc/v3/products/?sku=${element.sku}`, {
-          headers: {
-            'Authorization': 'Basic Y2tfNjQ3OGE0MTUzYTliNTE1ZjEzMjcwYzkwMTNhNTZlMTY5NmI4N2JkZDpjc18wOWJmZGNmNzY0NmZmNmNhNTVjZGYyNDUyMTE2Zjk0NDM4MzhiNjMx',
-            'Content-Type': 'application/json',
-            'Host': 'www.casaplant.ro',
-            'Connection': 'keep-alive',
-            'Accept-Encoding': 'gzip, deflate, br'
-          }
-        });
-        }) */
+    /* let promises = produseProcessed.map(element => {
+      return axios.get(`https://www.casaplant.ro/wp-json/wc/v3/products/?sku=${element.sku}`, {
+      headers: {
+        'Authorization': 'Basic Y2tfNjQ3OGE0MTUzYTliNTE1ZjEzMjcwYzkwMTNhNTZlMTY5NmI4N2JkZDpjc18wOWJmZGNmNzY0NmZmNmNhNTVjZGYyNDUyMTE2Zjk0NDM4MzhiNjMx',
+        'Content-Type': 'application/json',
+        'Host': 'www.casaplant.ro',
+        'Connection': 'keep-alive',
+        'Accept-Encoding': 'gzip, deflate, br'
+      }
+    });
+    }) */
 
-      await Promise.all(produseProcessed.filter(async element => {
-          let productFromCasaPlant = await axios.get(`https://www.casaplant.ro/wp-json/wc/v3/products/?sku=${element.sku}`, {
-          headers: {
-            'Authorization': 'Basic Y2tfNjQ3OGE0MTUzYTliNTE1ZjEzMjcwYzkwMTNhNTZlMTY5NmI4N2JkZDpjc18wOWJmZGNmNzY0NmZmNmNhNTVjZGYyNDUyMTE2Zjk0NDM4MzhiNjMx',
-            'Content-Type': 'application/json',
-            'Host': 'www.casaplant.ro',
-            'Connection': 'keep-alive',
-            'Accept-Encoding': 'gzip, deflate, br'
-          }
-        });
-        if(productFromCasaPlant.data.length === 0){
-          let category = await axios.get(`https://www.casaplant.ro/wp-json/wc/v3/products/categories/?search=${element.denumire_grupa}`, {
-          headers: {
-            'Authorization': 'Basic Y2tfNjQ3OGE0MTUzYTliNTE1ZjEzMjcwYzkwMTNhNTZlMTY5NmI4N2JkZDpjc18wOWJmZGNmNzY0NmZmNmNhNTVjZGYyNDUyMTE2Zjk0NDM4MzhiNjMx',
-            'Content-Type': 'application/json',
-            'Host': 'www.casaplant.ro',
-            'Connection': 'keep-alive',
-            'Accept-Encoding': 'gzip, deflate, br'
-          }
-        });
-
-          if(category.data.length === 0){
-            
-          }
-          else{
-            if(element.sku === '709'){
-              let productToInsert = {
-                name: element.denumire_scurta,
-                sku: element.sku,
-                regular_price: element.pret_vanzare_amanunt,
-                stock_quantity: element.stoc,
-                categories: [
-                  {
-                    id: category.data[0].id
-                  }
-                ],
-                status: element.status === 'Inactiv' ? 'draft' : 'publish'
-              }
-             //console.log(productToInsert)
-
-              let insertedProduct = await axios.post(`https://www.casaplant.ro/wp-json/wc/v3/products/products/`, {
-          headers: {
-            'Authorization': 'Basic Y2tfNjQ3OGE0MTUzYTliNTE1ZjEzMjcwYzkwMTNhNTZlMTY5NmI4N2JkZDpjc18wOWJmZGNmNzY0NmZmNmNhNTVjZGYyNDUyMTE2Zjk0NDM4MzhiNjMx',
-            'Content-Type': 'application/json',
-            'Host': 'www.casaplant.ro',
-            'Connection': 'keep-alive',
-            'Accept-Encoding': 'gzip, deflate, br'
-          }
-        },
-        productToInsert
-        );
-              console.log(insertedProduct);
-            }
-          }
-          // search category
-          // if not existent => create category
-          // insert product
+    await Promise.all(produseProcessed.filter(async element => {
+      let productFromCasaPlant = await axios.get(`https://www.casaplant.ro/wp-json/wc/v3/products/?sku=${element.sku}`, {
+        headers: {
+          'Authorization': 'Basic Y2tfNjQ3OGE0MTUzYTliNTE1ZjEzMjcwYzkwMTNhNTZlMTY5NmI4N2JkZDpjc18wOWJmZGNmNzY0NmZmNmNhNTVjZGYyNDUyMTE2Zjk0NDM4MzhiNjMx',
+          'Content-Type': 'application/json',
+          'Host': 'www.casaplant.ro',
+          'Connection': 'keep-alive',
+          'Accept-Encoding': 'gzip, deflate, br'
         }
-        else{
-         // console.log("else: ", element.denumire_scurta)
-
-          // update product 
-        }
-        return element;
-        })) 
-
-     /*  let productsUnmatched = await Promise.all(promises).then(values => {
-        let newValues = [];
-         values.forEach(val => {
-          if(val.data.length === 0){
-            newValues.push(val.data[0]);
-          }
-        })
-        console.log(newValues)
-      }) */
-
-
-
-        return produseProcessed;
       });
+      if (productFromCasaPlant.data.length === 0) {
+        let category = await axios.get(`https://www.casaplant.ro/wp-json/wc/v3/products/categories/?search=${element.denumire_grupa}`, {
+          headers: {
+            'Authorization': 'Basic Y2tfNjQ3OGE0MTUzYTliNTE1ZjEzMjcwYzkwMTNhNTZlMTY5NmI4N2JkZDpjc18wOWJmZGNmNzY0NmZmNmNhNTVjZGYyNDUyMTE2Zjk0NDM4MzhiNjMx',
+            'Content-Type': 'application/json',
+            'Host': 'www.casaplant.ro',
+            'Connection': 'keep-alive',
+            'Accept-Encoding': 'gzip, deflate, br'
+          }
+        });
+
+        if (category.data.length === 0) {
+
+        }
+        else {
+          if (element.sku === '709') {
+            let productToInsert = {
+              name: element.denumire_scurta,
+              sku: element.sku,
+              regular_price: element.pret_vanzare_amanunt,
+              stock_quantity: element.stoc,
+              categories: [
+                {
+                  id: category.data[0].id
+                }
+              ],
+              status: element.status === 'Inactiv' ? 'draft' : 'publish'
+            }
+            //console.log(productToInsert)
+
+            let insertedProduct = await axios.post(`https://www.casaplant.ro/wp-json/wc/v3/products/`, productToInsert, {
+              headers: {
+                'Authorization': 'Basic Y2tfNjQ3OGE0MTUzYTliNTE1ZjEzMjcwYzkwMTNhNTZlMTY5NmI4N2JkZDpjc18wOWJmZGNmNzY0NmZmNmNhNTVjZGYyNDUyMTE2Zjk0NDM4MzhiNjMx',
+                'Content-Type': 'application/json',
+                'Host': 'www.casaplant.ro',
+                'Connection': 'keep-alive',
+                'Accept-Encoding': 'gzip, deflate, br'
+              }
+            }
+            );
+            console.log(insertedProduct);
+          }
+        }
+        // search category
+        // if not existent => create category
+        // insert product
+      }
+      else {
+        // console.log("else: ", element.denumire_scurta)
+
+        // update product 
+      }
+      return element;
+    }))
+
+    /*  let productsUnmatched = await Promise.all(promises).then(values => {
+       let newValues = [];
+        values.forEach(val => {
+         if(val.data.length === 0){
+           newValues.push(val.data[0]);
+         }
+       })
+       console.log(newValues)
+     }) */
+
+
+
+    return produseProcessed;
+  });
 }
 
 getProducts();
